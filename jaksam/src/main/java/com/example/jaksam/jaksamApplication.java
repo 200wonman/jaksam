42a880edc964c3a18ae5d174dae566ba5632cdf5
juye <kjy_1615@naver.com>
2023-04-18 09:43:37 +0900

    hi

---
+++ jaksam/src/main/java/com/example/jaksam/jaksamApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaksam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jaksamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(jaksamApplication.class, args);
+	}
+
+}
